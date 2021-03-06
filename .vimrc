94d609118f3b1154d1fce45486b1531a19a81814
jaimevp54 <jaimevp54@gmail.com>
2021-03-06 03:05:30 -0500

    Initial commit

---
+++ .vimrc
@@ -0,0 +1,1 @@
+source ~/.sys-config/vim/.vimrc
---
+++ setup.sh
@@ -0,0 +1,18 @@
+echo "Setting up i3"
+echo "Creating .configs/.i3 directory "
+mkdir ~/.config/i3
+echo "Creating config hard link"
+ln i3/config ~/.config/i3/config
+
+echo "Setting up Vim"
+echo "source ~/.sys-config/vim/.vimrc" >> ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "Running plugin install"
+vim +PluginInstall +qall
+
+
+echo "Setting up git"
+
+echo "Creating .gitconfig hard link"
+ln git/.gitconfig ~/.gitconfig
